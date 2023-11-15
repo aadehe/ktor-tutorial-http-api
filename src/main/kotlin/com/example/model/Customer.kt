ab24c85825b6ba9d0fed0b8f13382c81dafcab99
albertad <aadehe@gmail.com>
2023-11-15 16:39:49 +0000

    Add Customer model and customer routing

--- src/main/kotlin/com/example/model/Customer.kt
+++ src/main/kotlin/com/example/model/Customer.kt
@@ -4,8 +4,5 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Customer(val id: String, val firstName: String, val lastName: String, val email: String)
-val customerStorage = mutableListOf<Customer>(
-    Customer("1", "Albert", "Adehe", "aadehe@gmail.com"),
-    Customer("2", "John", "Sylvester", "jsylvester@yahoo.com")
-)
+val customerStorage = mutableListOf<Customer>()
 
